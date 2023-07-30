3dd45e521dff47c8e4f9b12aec37ebcb6c31f9d5
Alex Kretzschmar <alexktz@gmail.com>
2023-07-30 14:23:34 -0400

    script

---
+++ data/iterm2/install.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# copies this plist to ~/.config/iterm2/
+# so that changes between hosts don't conflict
+
+mkdir -p ~/.config/iterm2/
+cp ./com.googlecode.iterm2.plist ~/.config/iterm2/
