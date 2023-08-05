4d6776b0c9efa0be3233dba0b139cc6a949a325d
Alex Kretzschmar <alexktz@gmail.com>
2023-08-05 18:35:15 -0400

    hacking

--- flake.nix
+++ flake.nix
@@ -10,52 +10,117 @@
       nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  # https://github.com/yusefnapora/nixos-system-flake
   outputs = inputs@{ self, nixpkgs, nixpkgs-unstable, home-manager, nix-darwin, ... }:
-  let
-    inherit (nixpkgs.lib) nixosSystem lists;
-    inherit (nix-darwin.lib) darwinSystem;
-    
-    mkDarwinConfig = { system, modules, ... }:
-      darwinSystem {
+    let  
+      genPkgs = system: import nixpkgs {
         inherit system;
-
-        inputs = { inherit nix-darwin home-manager nixpkgs nixpkgs-unstable; };
-        modules = modules
-        ++ [
-          home-manager.darwinModules.home-manager {
-            home-manager.useGlobalPkgs = true;
-            home-manager.useUserPackages = true;
-
-            home-manager.users.alex = {
-              imports = [
-                ./.config/darwin/home.nix
-              ];
-            };
-
-            home-manager.extraSpecialArgs = {
-              inherit inputs system;
-              nixosConfig = {};
-              unstablePkgs = inputs.nixpkgs-unstable.legacyPackages.aarch64-darwin;
-            };
-          }
-        ];
+        config.allowUnfree = true;
       };
-  in
-  {
-    darwinConfigurations = {
-      slartibartfast = mkDarwinConfig {
-        system = "aarch64-darwin";
-        modules = [
-          ./.config/darwin/darwin-configuration.nix
-        ];
+      genDarwinPkgs = system: import nixpkgs-darwin {
+        inherit system;
+        config.allowUnfree = true;
       };
-      magrathea = mkDarwinConfig {
-        system = "aarch64-darwin";
-        modules = [
-          ./.config/darwin/darwin-configuration.nix
-        ];
+    
+    
+      nixosSystem = hostName: system:
+        let
+          pkgs = genPkgs system;
+        in
+          nixpkgs.lib.nixosSystem {
+            inherit system;
+            modules = [
+
+            ];
+          };
+
+      darwinSystem = system: hostName: username:
+        let
+          pkgs = genDarwinPkgs system;
+        in
+          darwin.lib.darwinSystem {
+            inherit system;
+
+            inputs = { inherit nix-darwin home-manager nixpkgs nixpkgs-unstable; };
+            modules = [
+              home-manager.darwinModules.home-manager 
+              {
+                networking.hostName = hostName;
+                home-manager.useGlobalPkgs = true;
+                home-manager.useUserPackages = true;
+                home-manager.users.{$username} = {
+                  imports = [
+                    ./.config/darwin/home.nix
+                  ];
+                };
+                home-manager.extraSpecialArgs = {
+                  inherit inputs system;
+                  #nixosConfig = {};
+                  unstablePkgs = inputs.nixpkgs-unstable.legacyPackages.aarch64-darwin;
+                };
+              }
+              #./darwin-common.nix
+            ];
+          };
+
+      processConfigurations = lib.mapAttrs (n: v: v n);
+
+    in
+    {
+      darwinConfigurations = processConfigurations {
+        magrathea = darwinSystem "aarch64-darwin" [ ./hosts/magrathea/] "alex";
       };
     };
+
   };
+
 }
+
+  # # https://github.com/yusefnapora/nixos-system-flake
+  # outputs = inputs@{ self, nixpkgs, nixpkgs-unstable, home-manager, nix-darwin, ... }:
+  # let
+  #   inherit (nixpkgs.lib) nixosSystem lists;
+  #   inherit (nix-darwin.lib) darwinSystem;
+    
+  #   mkDarwinConfig = { system, modules, ... }:
+  #     darwinSystem {
+  #       inherit system;
+
+  #       inputs = { inherit nix-darwin home-manager nixpkgs nixpkgs-unstable; };
+  #       modules = modules
+  #       ++ [
+  #         home-manager.darwinModules.home-manager {
+  #           home-manager.useGlobalPkgs = true;
+  #           home-manager.useUserPackages = true;
+
+  #           home-manager.users.alex = {
+  #             imports = [
+  #               ./.config/darwin/home.nix
+  #             ];
+  #           };
+
+  #           home-manager.extraSpecialArgs = {
+  #             inherit inputs system;
+  #             nixosConfig = {};
+  #             unstablePkgs = inputs.nixpkgs-unstable.legacyPackages.aarch64-darwin;
+  #           };
+  #         }
+  #       ];
+  #     };
+  # in
+  # {
+  #   darwinConfigurations = {
+  #     slartibartfast = mkDarwinConfig {
+  #       system = "aarch64-darwin";
+  #       modules = [
+  #         ./.config/darwin/darwin-configuration.nix
+  #       ];
+  #     };
+  #     magrathea = mkDarwinConfig {
+  #       system = "aarch64-darwin";
+  #       modules = [
+  #         ./.config/darwin/darwin-configuration.nix
+  #       ];
+  #     };
+  #   };
+  # };
+#}
