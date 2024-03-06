9d2c54c4412ef9ec98cc87a8e5f7b369b1fdab2e
Alex Kretzschmar <alexktz@gmail.com>
2024-03-06 10:57:02 -0500

    common packages now a module

--- hosts/darwin/tsMBA/default.nix
+++ hosts/darwin/tsMBA/default.nix
@@ -1,9 +1,4 @@
-{ config, pkgs, unstablePkgs, ... }:
-
-{
-  environment.systemPackages = import ./../../common/common-packages.nix
-    {
-      pkgs = pkgs;
-      unstablePkgs = unstablePkgs;
-    };
-}
+{ config, ... }:
+  imports = [
+      ./../../common/common-packages.nix
+    ];
