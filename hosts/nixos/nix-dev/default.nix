19050f30c0b3db96406c173f60e36836ce3fa5d1
Alex Kretzschmar <alexktz@gmail.com>
2024-03-24 12:38:25 -0400

    adds gnome

--- hosts/nixos/nix-dev/default.nix
+++ hosts/nixos/nix-dev/default.nix
@@ -27,6 +27,20 @@
   services.vscode-server.enable = true;
   services.tailscale.enable = true;
 
+  services.xserver.enable = true;
+  services.xserver.displayManager.gdm.enable = true;
+  services.xserver.desktopManager.gnome.enable = true;
+
+  sound.enable = true;
+  hardware.pulseaudio.enable = false;
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+  };
+
   virtualisation = {
     docker = {
       enable = true;
