66bb09d332ca70a879e6fa72765c9be81b485040
Alex Kretzschmar <alexktz@gmail.com>
2024-07-15 19:24:45 -0400

    basic

---
+++ flake.nix
@@ -0,0 +1,11 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+    nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
+
+    vscode-server.url = "github:nix-community/nixos-vscode-server";
+  };
+
+  outputs = { nixpkgs, ... }@inputs:
+
+};
