314a0ee687e1e20d612c756d53e8ee49255d3051
Alex Kretzschmar <alexktz@gmail.com>
2024-09-15 23:14:59 -0400

    cleanup

--- hosts/nixos/framework/plasma6.nix
+++
@@ -1,8 +0,0 @@
-{ inputs, ... }:
-{
-  disabledModules = [ "services/x11/desktop-managers/plasma5.nix" ];
-
-  imports = [
-    "${inputs.nixpkgs-unstable}/nixos/modules/services/x11/desktop-managers/plasma6.nix"
-  ];
-}
