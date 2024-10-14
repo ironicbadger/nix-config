5807019374cfc111d7981ccae52c46684fe9ed45
Alex Kretzschmar <alexktz@gmail.com>
2024-10-14 19:17:24 -0400

    baldrick custom

--- common/darwin-common-dock.nix
+++ common/darwin-common-dock.nix
@@ -2,10 +2,19 @@
 {
   system.defaults.dock = {
     persistent-apps = [
+      "/Applications/Firefox.app"
       "/Applications/Google Chrome.app"
       "/Applications/Telegram.app"
       "/Applications/Discord.app"
+      "/Applications/Element.app"
+      "/Applications/Ivory.app"
+      "/Applications/Slack.app"
+      "/Applications/Obsidian.app"
+      "/Applications/Fantastical.app"
       "/Applications/Visual Studio Code.app"
+      "/Applications/Spotify.app"
+      "/Applications/Plexamp.app"
+      "/Applications/iTerm.app"
       "/Applications/Alacritty.app"
     ];
   };
--- hosts/darwin/baldrick/custom-dock.nix
+++ hosts/darwin/baldrick/custom-dock.nix
@@ -5,17 +5,10 @@
       "/Applications/Firefox.app"
       "/Applications/Google Chrome.app"
       "/Applications/Telegram.app"
-      "/Applications/Discord.app"
-      "/Applications/Element.app"
-      "/Applications/Ivory.app"
-      "/Applications/Slack.app"
       "/Applications/Obsidian.app"
-      "/Applications/Fantastical.app"
       "/Applications/Visual Studio Code.app"
-      "/Applications/Spotify.app"
-      "/Applications/Plexamp.app"
+      "/Applications/OBS.app"
       "/Applications/iTerm.app"
-      "/Applications/Alacritty.app"
     ];
   };
 }
