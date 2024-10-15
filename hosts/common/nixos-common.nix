d319b6d5fc7730c030a67c7e0e5661a83218bb76
Alex Kretzschmar <2773080+ironicbadger@users.noreply.github.com>
2024-10-14 20:50:52 -0400

    Colmena mainline (#10)
    * colmena ktz-cloud
    
    * rename
    
    * slim jim
    
    * working

--- hosts/common/nixos-common.nix
+++ hosts/common/nixos-common.nix
@@ -1,9 +1,20 @@
-{ pkgs, unstablePkgs, lib, inputs, ... }:
+{ pkgs, unstablePkgs, lib, inputs, stateVersion, ... }:
 let
   inherit (inputs) nixpkgs nixpkgs-unstable;
 in
 {
   time.timeZone = "America/New_York";
+  system.stateVersion = stateVersion;
+
+  virtualisation = {
+    docker = {
+      enable = true;
+      autoPrune = {
+        enable = true;
+        dates = "weekly";
+      };
+    };
+  };
 
   nix = {
     settings = {
@@ -17,36 +28,8 @@ in
       options = "--delete-older-than 5";
     };
   };
-  # nix.distributedBuilds = true;
-  # # Speeds things up by downloading dependencies remotely:
-  # nix.extraOptions = ''
-  #   builders-use-substitutes = true
-  # '';
 
-  #nixpkgs.config.allowUnfree = true;
-  # nixpkgs.config.permittedInsecurePackages = [
-  #   "electron-24.8.6"
+  # environment.systemPackages = with pkgs; [
+  #   #
   # ];
-
-  environment.systemPackages = with pkgs; [
-    # intel-gpu-tools
-    # libva-utils
-    # intel-media-driver
-    # jellyfin-ffmpeg
-    # hddtemp
-    # synergy
-  ];
-
-  ## pins to stable as unstable updates very often
-  # nix.registry.nixpkgs.flake = inputs.nixpkgs;
-  # nix.registry = {
-  #   n.to = {
-  #     type = "path";
-  #     path = inputs.nixpkgs;
-  #   };
-  #   u.to = {
-  #     type = "path";
-  #     path = inputs.nixpkgs-unstable;
-  #   };
-  # };
 }
---
+++ hosts/nixos/ktz-cloud/hardware-configuration.nix
@@ -0,0 +1,13 @@
+{ modulesPath, ... }:
+{
+  imports = [ (modulesPath + "/profiles/qemu-guest.nix") ];
+  boot.loader.grub = {
+    efiSupport = true;
+    efiInstallAsRemovable = true;
+    device = "nodev";
+  };
+  fileSystems."/boot" = { device = "/dev/disk/by-uuid/B7B8-3A41"; fsType = "vfat"; };
+  boot.initrd.availableKernelModules = [ "ata_piix" "uhci_hcd" "xen_blkfront" "vmw_pvscsi" ];
+  boot.initrd.kernelModules = [ "nvme" ];
+  fileSystems."/" = { device = "/dev/sda1"; fsType = "ext4"; };
+}
