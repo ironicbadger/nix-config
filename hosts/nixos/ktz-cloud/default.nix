9a7e2aab3cab531ecea036d8520781b02d2f1dc2
Alex Kretzschmar <alexktz@gmail.com>
2024-10-19 18:21:26 -0400

    figurine banner tweak

--- hosts/nixos/ktz-cloud/default.nix
+++ hosts/nixos/ktz-cloud/default.nix
@@ -31,7 +31,11 @@
 
   users.users.root.openssh.authorizedKeys.keys = [''ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIM7aI+bMjxTx7L9FRzJlk4UFCyYPzHs9Xs+vAhvPEYtk'' ''ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILkK1tv8BNtQJFt+n5yOJf6TQ/Ms9WkRi56MpyZOlWIk'' ''ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIA2bSgQvelWzGLh4v1nv+OYx8YNijAikvVq4E9qXDWYN'' ''ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAII+F3XpAIh4l8GfPgwoTqWQj0OdZRnnG9Ak4Z0wu0Upj'' ];
   users.defaultUserShell = pkgs.bash;
-  programs.bash.interactiveShellInit = "figurine -f \"3d.flf\" ktz-cloud";
+  programs.bash.interactiveShellInit = ''
+    echo \"\"
+    figurine -f \"3d.flf\" ktz-cloud"
+    echo \"\"
+  '';
 
   environment.systemPackages = with pkgs; [
     ansible
