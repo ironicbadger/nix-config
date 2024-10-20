086893d3d41c79c9ae12368778c2213f1dbc9a9e
Alex Kretzschmar <alexktz@gmail.com>
2024-10-20 19:22:37 -0400

    indentation

--- flake.nix
+++ flake.nix
@@ -1,22 +1,22 @@
 {
   inputs = {
-      nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
-      nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
-      nixpkgs-darwin.url = "github:NixOS/nixpkgs/nixpkgs-24.05-darwin";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+    nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs-darwin.url = "github:NixOS/nixpkgs/nixpkgs-24.05-darwin";
 
-      nix-darwin.url = "github:lnl7/nix-darwin";
-      nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
+    nix-darwin.url = "github:lnl7/nix-darwin";
+    nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
 
-      nix-homebrew.url = "github:zhaofengli-wip/nix-homebrew";
-      homebrew-core = { url = "github:homebrew/homebrew-core"; flake = false; };
-      homebrew-cask = { url = "github:homebrew/homebrew-cask"; flake = false; };
-      homebrew-bundle = { url = "github:homebrew/homebrew-bundle"; flake = false; };
+    nix-homebrew.url = "github:zhaofengli-wip/nix-homebrew";
+    homebrew-core = { url = "github:homebrew/homebrew-core"; flake = false; };
+    homebrew-cask = { url = "github:homebrew/homebrew-cask"; flake = false; };
+    homebrew-bundle = { url = "github:homebrew/homebrew-bundle"; flake = false; };
 
-      home-manager.url = "github:nix-community/home-manager/release-24.05";
-      home-manager.inputs.nixpkgs.follows = "nixpkgs";
+    home-manager.url = "github:nix-community/home-manager/release-24.05";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
 
-      sops-nix.url = "github:Mic92/sops-nix";
-      sops-nix.inputs.nixpkgs.follows = "nixpkgs";
+    sops-nix.url = "github:Mic92/sops-nix";
+    sops-nix.inputs.nixpkgs.follows = "nixpkgs";
 
       # disko.url = "github:nix-community/disko";
       # disko.inputs.nixpkgs.follows = "nixpkgs";
