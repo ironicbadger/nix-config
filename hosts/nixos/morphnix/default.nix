fc4fc854c8e4624d8c6c704ab2ce83dd294aa93f
Alex Kretzschmar <alexktz@gmail.com>
2024-12-22 09:46:26 -0500

    route change from main to local

--- hosts/nixos/morphnix/default.nix
+++ hosts/nixos/morphnix/default.nix
@@ -97,7 +97,7 @@
     defaultGateway = "10.42.0.254";
     nameservers = [ "10.42.0.253" ];
   localCommands = ''
-    ip rule add to 10.42.0.0/21 priority 2500 lookup main
+    ip rule add to 10.42.0.0/21 priority 2500 lookup local
   '';
   };
 
