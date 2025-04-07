e18e592907bc30588916ca0a6c81fc10aef8a7d9
Alex Kretzschmar <alexktz@gmail.com>
2025-04-07 00:26:11 -0400

    beszel

---
+++ hosts/nixos/morphnix/beszel.nix
@@ -0,0 +1,26 @@
+{
+  users.users.beszel = {
+    isSystemUser = true;
+    group = "beszel";
+    description = "Beszel Agent service user";
+  };
+  users.groups.beszel = {};
+
+  systemd.services.beszel-agent = {
+    description = "Beszel Agent Service";
+    after = [ "network.target" ];
+    wantedBy = [ "multi-user.target" ];
+
+    serviceConfig = {
+      Environment = [
+        "PORT=45876"
+        ''KEY="ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFIkr64nTWbuhU7l+VrLO7lPDRgh2LVqTtrIberNge1j"''
+        "EXTRA_FILESYSTEMS=/mnt/jbod,/rpool,/mnt/bigrust18"
+      ];
+      ExecStart = "/run/current-system/sw/bin/beszel-agent";
+      User = "beszel";
+      Restart = "always";
+      RestartSec = 5;
+    };
+  };
+}
