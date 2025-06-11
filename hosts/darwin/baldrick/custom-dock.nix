9e21e8cb9ef287b0369f3b763fb1d569b955138b
Alex Kretzschmar <alexktz@gmail.com>
2025-06-11 13:13:50 -0400

    baldrick dock

--- hosts/darwin/baldrick/custom-dock.nix
+++ hosts/darwin/baldrick/custom-dock.nix
@@ -4,6 +4,7 @@
     persistent-apps = [
       "/Applications/Google Chrome.app"
       "/Applications/Telegram.app"
+      "/Applications/Slack.app"
       "/Applications/Obsidian.app"
       "/Applications/Visual Studio Code.app"
       "/Applications/OBS.app"
