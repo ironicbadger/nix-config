57795c420b7c9a7f08b2588c36a26c48e4e7b4c0
Alex Kretzschmar <alexktz@gmail.com>
2026-03-02 09:53:42 -0500

    removes HAC as it is chatty when changing networks

--- hosts/common/darwin-common.nix
+++ hosts/common/darwin-common.nix
@@ -177,7 +177,7 @@ in
       "Disk Speed Test" = 425264550;
       "Fantastical" = 975937182;
       "Ivory for Mastodon by Tapbots" = 6444602274;
-      "Home Assistant Companion" = 1099568401;
+      #"Home Assistant Companion" = 1099568401;
       #"Microsoft Remote Desktop" = 1295203466;
       "Perplexity" = 6714467650;
       "Resize Master" = 1025306797;
